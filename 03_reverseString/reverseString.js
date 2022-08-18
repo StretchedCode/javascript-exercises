dd2738416fb956e25c0986f8dde7e3bd375b1020
Ghufran <ghufranshahid101@gmail.com>
2022-08-18 00:09:23 -0400

    Passed all test cases

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,15 @@
-const reverseString = function() {
+const reverseString = function(string) {
 
+let reverse = '';
+
+for (let i = string.length - 1; i >= 0; i--){
+    reverse += string.charAt(i);
+}
+
+return reverse;
 };
+
+console.log(reverseString('hello world'))
 
 // Do not edit below this line
 module.exports = reverseString;
