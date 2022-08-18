6612237672e2d5d01d933e7f5e88b03b8cfdcfe1
Ghufran <ghufranshahid101@gmail.com>
2022-08-18 02:29:09 -0400

    Passed all test cases

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,10 +1,16 @@
-const ftoc = function() {
+const ftoc = function(temperature) {
 
+  let newTemp = Math.round(((temperature - 32) * (5/9)) * 10) / 10;
+  return newTemp;
 };
 
-const ctof = function() {
+const ctof = function(temperature) {
 
+  let newTemp = Math.round(((temperature * (9/5)) + 32) * 10) / 10;
+  return newTemp;
 };
+
+console.log(ftoc(100));
 
 // Do not edit below this line
 module.exports = {
