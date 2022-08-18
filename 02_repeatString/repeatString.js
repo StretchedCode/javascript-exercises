898dbeaedb90e2f942bd799743766437c42a6abf
Ghufran <ghufranshahid101@gmail.com>
2022-08-17 23:52:30 -0400

    Created for loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repeat) {
 
+let newString = '';
+
+    for (let i = 0; i < repeat; i++){
+        newString += string;
+    }
+    return newString;
 };
+
+console.log(repeatString('amogus', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
